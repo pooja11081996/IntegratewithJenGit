cdd58655c199d2844cee067711fe1b63eae28f48
aaradhyan <Aaradhya.Nachankar@tapasi.com>
2024-09-30 10:25:17 +0530

    By Me (github Jenkins Integration)

---
+++ GithubJenkinsIntegration/src/com/cjc/integrate/Integration.java
@@ -0,0 +1,29 @@
+package com.cjc.integrate;
+
+public class Integration
+{
+public static void main(String[] args) 
+{
+	System.out.println("Hello");
+}
+public void  m1()
+{
+	System.out.println("Hiii method 1");
+}
+public void m2()
+{
+	System.out.println("Hiii method 2");
+}
+public void m3()
+{
+	System.out.println("Hiii method 3");
+}
+public void m4()
+{
+	System.out.println("Hiii method 4");
+}
+public void m5()
+{
+	System.out.println("Hiii method 5");
+}
+}
